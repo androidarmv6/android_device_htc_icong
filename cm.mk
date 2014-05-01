87c13f946af8f37dd99d7775929e189b2929caf4
olivieer <y.karasangabo@bg-rams.ac.at>
2014-05-01 13:51:31 +0000

    icong: initial KK bringup
    Signed-off-by: olivieer <y.karasangabo@bg-rams.ac.at>

--- cm.mk
+++ cm.mk
@@ -19,5 +19,11 @@ TARGET_SCREEN_WIDTH := 320
 PRODUCT_NAME := cm_icong
 PRODUCT_DEVICE := icong
 
+# Release name and versioning
+PRODUCT_RELEASE_NAME := Salsa
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+BUILD_ID := KVT49L
+ROM_BUILDTYPE := HOMEMADE
+
 ## Assign the build fingerprint to the build.prop file.
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=cyanogenmod/htc_icong/icong:4.3.1/$(BUILD_ID)/v0.1:user/test-keys
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=cyanogenmod/htc_icong/icong:4.4.2/$(BUILD_ID)/v0.1:user/test-keys
