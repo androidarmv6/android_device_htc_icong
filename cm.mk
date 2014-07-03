2dcb496a02f6ce7e0ffd15bd0be16191da75d25f
olivieer <y.karasangabo@bg-rams.ac.at>
2014-07-03 05:16:27 -0400

    icong: cleanup cm.mk
    * gsm.mk doesn't (actually) exist, see:
    https://github.com/androidarmv6/android_vendor_cm/blob/cm-11.0/config/gsm.mk
    * There's no need for a custom build fingerprint
    * Remove weird versioning defines
    
    Change-Id: I5597950b33c1f2a88edbe7bebb393cf1626879ca

--- cm.mk
+++ cm.mk
@@ -1,14 +1,11 @@
 # Licensed under the Apache License, Version 2.0.
-# Copyright (C) 2009 The Android Open Source Project
-# Copyright (C) 2011 The CyanogenMod Project
-# Copyright (C) 2014 Benjamin Gwynn <http://xenxier.tk>
-# Re-written by Benjamin Gwynn
+# Copyright (C) 2014 The Android Open Source Project
+# Copyright (C) 2014 The CyanogenMod Project
 
 # Call our device's AOSP device config.
 $(call inherit-product, device/htc/icong/full_icong.mk)
 
 # Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/mini.mk)
 
 # Bootanimation
@@ -21,9 +18,3 @@ PRODUCT_DEVICE := icong
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := Salsa
-PRODUCT_VERSION_DEVICE_SPECIFIC :=
-BUILD_ID := KVT49L
-ROM_BUILDTYPE := HOMEMADE
-
-## Assign the build fingerprint to the build.prop file.
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=cyanogenmod/htc_icong/icong:4.4.2/$(BUILD_ID)/v0.1:user/test-keys
